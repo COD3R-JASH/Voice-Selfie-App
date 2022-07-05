a69e0fda04c5ae8940ded6e7a460c9c7f922d977
COD3R-JASH <101001262+COD3R-JASH@users.noreply.github.com>
2022-07-05 18:57:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ Webcam.set({
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = "<img id='selfie_image' src='"+data_uri+'">';
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src="+data_uri+">";
     });
 }
 
